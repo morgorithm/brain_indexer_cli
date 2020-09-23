90fdd10b8517e1e242b78f2502026cfffa87d76c
Jay Lee <jaylee.possible@gmail.com>
2020-09-24 00:30:26 +0900

    #15 closed

--- src/scripts/requests/card/index.js
+++ src/scripts/requests/card/index.js
@@ -2,42 +2,34 @@ import { UTIL } from '../../utils'
 
 export const CARD = {
   createCard: async (card) => {
-    try {
-      const res = await fetch('app/card/', {
-        method: 'POST',
-        body: JSON.stringify(card),
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFTOKEN': UTIL.getCSRFToken(),
-        },
-      })
+    const res = await fetch('app/card/', {
+      method: 'POST',
+      body: JSON.stringify(card),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFTOKEN': UTIL.getCSRFToken(),
+      },
+    })
 
-      if (!res.ok) {
-        throw new Error('Failed to create card')
-      }
-    } catch (e) {
-      throw e
+    if (!res.ok) {
+      alert(res.statusText)
     }
   },
 
   getRandomCard: async (categories) => {
     const categoriesName = categories.map((element) => element.name)
 
-    try {
-      const res = await fetch(
-        'app/random?' +
-          new URLSearchParams({
-            category: categoriesName,
-          })
-      )
+    const res = await fetch(
+      'app/random?' +
+        new URLSearchParams({
+          category: categoriesName,
+        })
+    )
 
-      if (!res.ok) {
-        throw new Error('Failed to get random card')
-      } else {
-        return await res.json()
-      }
-    } catch (e) {
-      throw e
+    if (!res.ok) {
+      alert(res.statusText)
+    } else {
+      return await res.json()
     }
   },
 }
